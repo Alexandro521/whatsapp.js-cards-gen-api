4c2319651c01806b329a511e5dcea366cc3cd6ca
Alexandro Mendez <elavion0151@gmail.com>
2024-12-01 18:34:44 -0400

    add cors config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import express from 'express';
-import {router} from './router/routes';
+import { router } from './router/routes';
+import  cors from 'cors';
 const app = express();
 
 app.disable('x-powered-by');
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
+
 app.use(express.json());
+
+app.use("/api", (req, res, next) => {
+  const {picture, kickname, exp, lvl, rank, status} = req.query;
+  if(picture && kickname && exp && lvl && rank && status) {
+    next();
+  } else {
+    res.status(400).json({
+      message: 'Bad Request',
+      error: 'Missing required parameters',
+      params: ['picture', 'kickname', 'exp', 'lvl', 'rank', 'status'],
+      status: 400,
+    });
+  }
+});
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
